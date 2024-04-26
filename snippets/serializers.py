d637cb6854e96032bbf213de669d7b21ecce19b4
omertairi04 <otairi010@gmail.com>
2024-04-27 00:39:18 +0200

    finsihed page 5 of rest docs

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -5,41 +5,18 @@ from django.contrib.auth.models import User
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    highlight = serializers.HyperlinkedIdentityField(view_name="snippet-highlight", format="html")
+    
     class Meta:
         model = Snippet
-        fields = ["id", "title", "code", "linenos", "language", "style", "owner"]
+        fields = ["id", "title", "code", "linenos", "language", "style", "owner", "highlight"]
 
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return Snippet.objects.create(**validated_data)
+   
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get("title", instance.title)
-        instance.code = validated_data.get("code", instance.code)
-        instance.linenos = validated_data.get("linenos", instance.linenos)
-        instance.language = validated_data.get("language", instance.language)
-        instance.style = validated_data.get("style", instance.style)
-        owner = serializers.ReadOnlyField(source="owner.username")
-        instance.save()
-        return instance
-
-    def delete(self, instance):
-        """
-        Delete the given `Snippet` instance.
-        """
-        instance.delete()
-
-
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all()
-    )
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
